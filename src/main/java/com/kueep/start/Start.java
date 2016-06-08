a98dd0bab003b7b72af134885461ed74f8d3186a
Martin Saad <martin.saad.ms@gmail.com>
2016-06-08 22:07:34 +0300

    change

--- src/main/java/com/kueep/start/Start.java
+++ src/main/java/com/kueep/start/Start.java
@@ -1,55 +1,55 @@
 package com.kueep.start;
-
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import org.apache.log4j.Logger;
-import org.quartz.JobBuilder;
-import org.quartz.JobDetail;
-import org.quartz.Scheduler;
-import org.quartz.SchedulerException;
-import org.quartz.SimpleScheduleBuilder;
-import org.quartz.Trigger;
-import org.quartz.TriggerBuilder;
-import org.quartz.impl.StdSchedulerFactory;
-
-import com.kueep.recommendation.Recommendation;
-
-public class Start {
-	
-	final static Logger logger = Logger.getLogger(Start.class);
-
-	public static void main(String[] args) throws SchedulerException, ParseException {
-		JobDetail job = JobBuilder.newJob().ofType(Recommendation.class)
-				.withIdentity("Recommendation Job", "group1").build();
-
-
-
-			// Trigger the job to run on the next round minute
-			Trigger trigger = TriggerBuilder
-				.newTrigger()
-				.withIdentity("dummyTriggerName", "group1")
-				.withSchedule(
-					SimpleScheduleBuilder.simpleSchedule()
-						.withIntervalInHours(24).repeatForever())
-				.build();
-
-			// schedule it
-			Scheduler scheduler = new StdSchedulerFactory().getScheduler();
-				
-			SimpleDateFormat dt = new SimpleDateFormat("yyyy-MM-dd");
-			
-			
-			Date[] arr = {getMeYesterday(), getMeYesterday()};
-			//Date[] arr = {dt.parse("2016-06-03"), dt.parse("2016-06-03")};
-			
-			scheduler.getContext().put("myContextVar", arr);
-			scheduler.start();
-			scheduler.scheduleJob(job, trigger);
-	}
-	
-	private static Date getMeYesterday(){
-	     return new Date(System.currentTimeMillis()-24*60*60*1000);
-	}
-}
+//
+//import java.text.ParseException;
+//import java.text.SimpleDateFormat;
+//import java.util.Date;
+//
+//import org.apache.log4j.Logger;
+//import org.quartz.JobBuilder;
+//import org.quartz.JobDetail;
+//import org.quartz.Scheduler;
+//import org.quartz.SchedulerException;
+//import org.quartz.SimpleScheduleBuilder;
+//import org.quartz.Trigger;
+//import org.quartz.TriggerBuilder;
+//import org.quartz.impl.StdSchedulerFactory;
+//
+//import com.kueep.recommendation.Recommendation;
+//
+//public class Start {
+//	
+//	final static Logger logger = Logger.getLogger(Start.class);
+//
+//	public static void main(String[] args) throws SchedulerException, ParseException {
+//		JobDetail job = JobBuilder.newJob().ofType(Recommendation.class)
+//				.withIdentity("Recommendation Job", "group1").build();
+//
+//
+//
+//			// Trigger the job to run on the next round minute
+//			Trigger trigger = TriggerBuilder
+//				.newTrigger()
+//				.withIdentity("dummyTriggerName", "group1")
+//				.withSchedule(
+//					SimpleScheduleBuilder.simpleSchedule()
+//						.withIntervalInHours(24).repeatForever())
+//				.build();
+//
+//			// schedule it
+//			Scheduler scheduler = new StdSchedulerFactory().getScheduler();
+//				
+//			SimpleDateFormat dt = new SimpleDateFormat("yyyy-MM-dd");
+//			
+//			
+//			Date[] arr = {getMeYesterday(), getMeYesterday()};
+//			//Date[] arr = {dt.parse("2016-06-03"), dt.parse("2016-06-03")};
+//			
+//			scheduler.getContext().put("myContextVar", arr);
+//			scheduler.start();
+//			scheduler.scheduleJob(job, trigger);
+//	}
+//	
+//	private static Date getMeYesterday(){
+//	     return new Date(System.currentTimeMillis()-24*60*60*1000);
+//	}
+//}
---
+++ src/main/java/launch/Main.java
@@ -0,0 +1,59 @@
+package launch;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
+
+import org.apache.catalina.WebResourceRoot;
+import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.webresources.DirResourceSet;
+import org.apache.catalina.webresources.StandardRoot;
+
+import com.kueep.recommendation.Recommendation;
+import org.apache.log4j.Logger;
+
+public class Main {
+	
+	final static Logger logger = Logger.getLogger(Main.class);
+
+    public static void main(String[] args) throws Exception {
+
+        String webappDirLocation = "src/main/webapp/";
+        Tomcat tomcat = new Tomcat();
+
+        //The port that we should run on can be set into an environment variable
+        //Look for that variable and default to 8080 if it isn't there.
+        String webPort = System.getenv("PORT");
+        if(webPort == null || webPort.isEmpty()) {
+            webPort = "8080";
+        }
+
+        tomcat.setPort(Integer.valueOf(webPort));
+
+        StandardContext ctx = (StandardContext) tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
+        System.out.println("configuring app with basedir: " + new File("./" + webappDirLocation).getAbsolutePath());
+
+        // Declare an alternative location for your "WEB-INF/classes" dir
+        // Servlet 3.0 annotation will work
+        File additionWebInfClasses = new File("target/classes");
+        WebResourceRoot resources = new StandardRoot(ctx);
+        resources.addPreResources(new DirResourceSet(resources, "/WEB-INF/classes",
+                additionWebInfClasses.getAbsolutePath(), "/"));
+        ctx.setResources(resources);
+
+        logger.debug("starting apache tomcat.......");
+        tomcat.start();
+        
+		SimpleDateFormat dt = new SimpleDateFormat("yyyy-MM-dd");
+		Date[] dates = {dt.parse("2016-06-03"), dt.parse("2016-06-03")};
+		Recommendation r = new Recommendation();
+		Map<String, String> preferences = r.setNewPreferences(dates[0], dates[1]);
+		if (preferences!=null)
+			r.saveNewPreferences(preferences);
+        
+        
+        tomcat.getServer().await();
+    }
+}
